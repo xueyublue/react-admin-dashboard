f6800abc493c476cd180cf5d4730263ca1a51bcf
Xue Yu <xueyublue@gmail.com>
2022-09-23 15:54:59 +0800

    Added chart to home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,13 @@
 import FeaturedInfo from "../../components/featuredInfo/FeaturedInfo";
 import "./home.css";
+import Chart from "./../../components/chart/Chart";
+import { userData } from "../../data.js";
 
 const Home = () => {
   return (
     <div className="home">
       <FeaturedInfo />
+      <Chart title="User Analytics" data={userData} dataKey={"Active User"} grid />
     </div>
   );
 };
