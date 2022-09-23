4ea35d8e9c410c64bce83fd9deb5d77d02469b8e
Xue Yu <xueyublue@gmail.com>
2022-09-23 17:39:49 +0800

    Added router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
+import React from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Topbar from "./components/topbar/Topbar";
 import Sidebar from "./components/sidebar/Sidebar";
 import "./app.css";
 import Home from "./pages/home/Home";
+import UserList from "./pages/userList/UserList";
 
 function App() {
   return (
-    <div>
+    <BrowserRouter>
       <Topbar />
       <div className="container">
         <Sidebar />
-        <Home />
+        <Routes>
+          <Route path="/" element={<Home />} exact></Route>
+          <Route path="/users" element={<UserList />}></Route>
+        </Routes>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
