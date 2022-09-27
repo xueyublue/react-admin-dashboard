7f43c0ddb79ca955f07de60811598965a703b716
Xue Yu <xueyublue@gmail.com>
2022-09-27 12:34:50 +0800

    Redirect to home page if path is /

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation, useNavigate } from "react-router-dom";
 import Topbar from "./components/topbar/Topbar";
 import Sidebar from "./components/sidebar/Sidebar";
 import "./app.css";
 import Home from "./pages/home/Home";
 import UserList from "./pages/userList/UserList";
+import { useEffect } from "react";
 
 function App() {
+  const history = useNavigate();
+  const { pathname } = useLocation();
+  useEffect(() => {
+    if (pathname && pathname === "/") {
+      history("/home");
+    }
+  }, []);
   return (
-    <BrowserRouter>
+    <>
       <Topbar />
       <div className="container">
         <Sidebar />
@@ -16,7 +24,7 @@ function App() {
           <Route path="/users" element={<UserList />}></Route>
         </Routes>
       </div>
-    </BrowserRouter>
+    </>
   );
 }
 
