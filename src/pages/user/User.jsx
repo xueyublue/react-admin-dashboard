96f8897577027dba2c0c9e17cb9a5fb11e2885e2
Xue Yu <xueyublue@gmail.com>
2022-09-29 16:04:14 +0800

    Updated user page

--- src/pages/user/User.jsx
+++ src/pages/user/User.jsx
@@ -1,7 +1,20 @@
 import "./user.css";
 
 const User = () => {
-  return <div className="user">User</div>;
+  return (
+    <div className="user">
+      <div className="userTitleContainer">
+        <h3>Edit User</h3>
+        <button className="userAddButton">Create</button>
+      </div>
+      <div className="userContainer">
+        <div className="userShow">Show</div>
+        <div className="userUpdate">
+          <h3>Edit</h3>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default User;
