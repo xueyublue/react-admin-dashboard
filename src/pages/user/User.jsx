5caa279e4fd2a3460a15fa03228366ba55218545
Xue Yu <xueyublue@gmail.com>
2022-09-29 16:13:40 +0800

    Updated user page

--- src/pages/user/User.jsx
+++ src/pages/user/User.jsx
@@ -8,7 +8,20 @@ const User = () => {
         <button className="userAddButton">Create</button>
       </div>
       <div className="userContainer">
-        <div className="userShow">Show</div>
+        <div className="userShow">
+          <div className="userShowTop">
+            <img
+              className="userShowTopImage"
+              src="https://images.unsplash.com/photo-1544005313-94ddf0286df2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80"
+              alt=""
+            />
+            <div className="userShowTopTitle">
+              <strong>Anna Becker</strong>
+              <span>Software Engineer</span>
+            </div>
+          </div>
+          <div className="userShowBottom"></div>
+        </div>
         <div className="userUpdate">
           <h3>Edit</h3>
         </div>
